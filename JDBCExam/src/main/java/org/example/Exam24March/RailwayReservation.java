5872ed14fc1aa8a436d5027f41295ab31336f62e
raajsuren <surendra.pranam@gmail.com>
2024-03-25 17:34:34 -0500

    exam

--- JDBCExam/src/main/java/org/example/Exam24March/RailwayReservation.java
+++ JDBCExam/src/main/java/org/example/Exam24March/RailwayReservation.java
@@ -1,9 +1,11 @@
 package org.example.Exam24March;
+import javax.sound.sampled.ReverbType;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
@@ -39,13 +41,44 @@ public class RailwayReservation {
             //select Statement
             Statement statement = connection.createStatement();
 
-            //Question 1.
-            String creatQuery= "create table RailWay_Reservastion(Passerger_Nmae varchar (50), Passenger_Age int (20), Choosean_Seat varchar (20), Reservation_Type varchar(20), IsSeniorCitizen boolean, Amount_Paid double)";
-             statement.execute(creatQuery);
-            System.out.println("Create table Successfully");
+//            //Question 1.
+//            String creatQuery= "create table RailWay_Reservastion(Passerger_Nmae varchar (50), Passenger_Age int (20), Choosean_Seat varchar (20), Reservation_Type varchar(20), IsSeniorCitizen boolean, Amount_Paid double)";
+//             statement.execute(creatQuery);
+//            System.out.println("Create table Successfully");
+
+            // insert data into railway_reservation table
+            System.out.println("Please provide Passenger_Name");
+            String Passenger_Name = scanner.next();
+
+            System.out.println("Please Provide Passenger_Age");
+            int Passenger_Age = scanner.nextInt();
+
+            System.out.println("Please Choose a Seat");
+            String Passenger_Seat = scanner.next();
+
+            System.out.println("Choose Reseravation Type(AC/ Non AC)");
+            String Type_Of_Reservation= scanner.next();
 
 
+            boolean IsSeniorCitizen = Passenger_Age>60;
 
+            double Amount_Paid= calculateAmount(Type_Of_Reservation);
+
+            // Insert from scanner
+            String query = "insert into RailWay_Reservastion values (?,?,?,?,?,?)";
+            PreparedStatement preparedStatement= connection.prepareStatement(query);
+
+            preparedStatement.setString(1, Passenger_Name);
+            preparedStatement.setInt(2, Passenger_Age);
+            preparedStatement.setString(3, Passenger_Seat);
+            preparedStatement.setString(4, Type_Of_Reservation);
+            if (IsSeniorCitizen){
+                preparedStatement.setInt(5, 1);
+            }
+            else {
+                preparedStatement.setInt(5, 0);
+            }
+            preparedStatement.setDouble(6,Amount_Paid);
 
 
         }
@@ -54,6 +87,14 @@ public class RailwayReservation {
 
         }
 
+        }
+    public static double calculateAmount(String type_Of_Reservation){
+        if (type_Of_Reservation.equalsIgnoreCase("AC")){
+            return 100.0;
 
+        }
+        else {
+            return 60.0;
+        }
     }
 }
